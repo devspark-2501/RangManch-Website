f674414fcbe593a239cbe694d8d22cba79302baa
Tanush Mathur <devspark2501@gmail.com>
2026-06-05 15:04:02 +0530

    Auth Page connected to User.js collection

--- front-end/lib/auth.js
+++ front-end/lib/auth.js
@@ -1,5 +1,8 @@
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";
+
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -13,4 +16,50 @@ export const authOptions = {
   },
 
   secret: process.env.AUTH_SECRET,
+
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await connectDB();
+
+        const existingUser = await User.findOne({
+          email: user.email,
+        });
+
+        if (!existingUser) {
+          await User.create({
+            name: user.name,
+            email: user.email,
+
+            // Google users don't have password
+            password: "GOOGLE_AUTH_USER",
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+
+    async session({ session }) {
+      try {
+        await connectDB();
+
+        const dbUser = await User.findOne({
+          email: session.user.email,
+        });
+
+        if (dbUser) {
+          session.user.id = dbUser._id.toString();
+        }
+
+        return session;
+      } catch (error) {
+        console.error(error);
+        return session;
+      }
+    },
+  },
 };
