5d70fdfea001bba695f867a508d58bdb0ef89943
Tanush Mathur <devspark2501@gmail.com>
2026-06-13 22:24:57 +0530

    gallery schema added

---
+++ front-end/models/Gallery.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const GallerySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Gallery ||
+  mongoose.model("Gallery", GallerySchema);
