8de6215ee31ebaa5255e6584089c2238d186e7d4
Tanush Mathur <devspark2501@gmail.com>
2026-06-22 01:12:45 +0530

    redirect route

--- front-end/app/Book-Stall/page.js
+++ front-end/app/Book-Stall/page.js
@@ -703,10 +703,10 @@ function BookStallContent() {
                     <span className="text-gray-700">
                       I agree to the exhibition{" "}
                       <a
-                        href="/read-more" target="_blank" rel="noopener noreferrer"
+                        href="/terms" target="_blank" rel="noopener noreferrer"
                         className="text-pink-600 underline hover:text-purple-600 transition font-medium"
                       >
-                        terms and conditions
+                        read more
                       </a>
                       .
                     </span>
